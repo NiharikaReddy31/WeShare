7b7b840deca648c7b6b1520309b4aed40186db42
Velmula Niharika <velmulaniharika31@gmail.com>
2020-09-08 11:53:54 +0530

    Implementing JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/User");
-const gravatar = require("gravatar"); 
+const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const { check, validationResult } = require("express-validator");
 
 // route        POST api/users
 // access       Public
 // description  Register User
- 
-router.post('/', 
+
+router.post('/',
     [
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Enter a valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
     ],
     async (req, res) => {
-        console.log(req.body);
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({
@@ -43,16 +44,30 @@ router.post('/',
             user = new User({
                 name,
                 email,
-                avatar,  
+                avatar,
                 password
-                
+
             });
 
             // Encrypt Password
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
             await user.save();
-            res.send("User Registered Successfully");
+
+            // return JSON WebToken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            jwt.sign(payload,
+                config.get("jwtSecret"),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token })
+                }
+            );
         }
         catch (err) {
             console.error(err.message)
