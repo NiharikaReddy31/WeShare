7a6e485bc48a9032540cf57e6651d28a9bb42166
Velmula Niharika <velmulaniharika31@gmail.com>
2020-09-20 11:27:07 +0530

    Loading Logged in user and setting auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment,useEffect} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
@@ -9,8 +9,22 @@ import Alert from "./components/layout/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
-const App = () => (
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  
+  
+  return(
   <Provider store={store}>
     <Router>
       <Fragment>
@@ -27,5 +41,5 @@ const App = () => (
     </Router>
   </Provider>
 );
+};
 export default App;
-// racf
