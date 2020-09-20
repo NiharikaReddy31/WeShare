b1141788a7facdc3e7f2d4f4bd42c28aff21d824
Velmula Niharika <velmulaniharika31@gmail.com>
2020-09-20 18:24:18 +0530

    Logout User

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,10 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS
+    AUTH_ERROR,
+     LOGIN_FAIL,
+     LOGIN_SUCCESS,
+     LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -35,6 +38,7 @@ export default function (state = initialState, action) {
         case REGISTER_FAIL:
         case AUTH_ERROR:
         case LOGIN_FAIL:
+            case LOGOUT:
             localStorage.removeItem("token");
             return {
                 ...state,
